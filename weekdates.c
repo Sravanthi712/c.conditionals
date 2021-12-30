56acd3c74b9397cb0e47617a8995540cce8284b1
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2021-12-30 15:47:08 +0530

    Add files via upload

---
+++ weekdates.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int x;
+	scanf("%d",&x);
+	if(x==1)
+	
+printf("monday");
+
+if(x==2)
+printf("tuesday");
+if(x==3)
+printf("wednesday");
+if(x==4)
+printf("thursady");
+if(x==5)
+printf("friday");
+if(x==6)
+printf("saturday");
+if(x==7)
+printf("sunday");
+else
+printf("invalid input");
+}
