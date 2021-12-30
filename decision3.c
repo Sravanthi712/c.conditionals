311e6a88ab1f6990f34fe364dcb2825d261870af
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2021-12-30 15:45:56 +0530

    Add files via upload

---
+++ decision3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int cp,sp;
+	scanf("%d%d",&cp,&sp);
+	if(cp<sp)
+	{
+	printf("profit"); 
+}
+	 else if(cp>sp)
+
+	 {
+	 printf("loss");}
+	
+	else
+{
+		printf("no profit no loss");
+
+}}
