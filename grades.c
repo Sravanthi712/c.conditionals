0cfe2fa047e3e647a2f059836bc6bef32b74aa8a
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2021-12-31 15:06:32 +0530

    Add files via upload

---
+++ grades.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int marks;
+	scanf("%d",&marks);
+	if(90<=marks&&marks<=100)
+	{
+		printf("0 grade");
+		}
+	else if(80<=marks&&marks<90)
+	{
+		printf("A grade");
+	}
+	else if(70<=marks&&marks<80)
+	{
+		printf("B grade");
+	}
+	else if(60<=marks&&marks<70)
+	{
+		printf("C grade");
+	}
+	else if(50<=marks&&marks<60)
+	{
+		printf("D grade");
+	}
+else
+{
+	printf("not qualified");
+}
+}
