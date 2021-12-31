8ece72a031be44bbae08d0ea3bca5bae9cdcb539
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2021-12-31 15:06:01 +0530

    Add files via upload

---
+++ divisible.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int x;
+	scanf("%d",&x);
+	if(x%1==0)
+	printf("divisible by 1");
+	if(x%2==0)
+	printf("\ndivisible by 2");
+	if(x%3==0)
+	printf("\ndivisible by 3");
+	if(x%4==0)
+	printf("\ndivisible by 4");
+	if(x%5==0)
+	printf("\ndivisible by 5");
+	
+}
